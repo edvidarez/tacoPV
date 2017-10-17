f41ea85609958eb9162de88fbed14c35e812523a
Pasos <ali.pasos@arusoft.com>
2017-10-17 04:38:47 -0500

    controllers

--- src/FXML/facturarXmlController.java
+++ src/FXML/facturarXmlController.java
@@ -3,7 +3,27 @@ package FXML;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class facturarXmlController implements javafx.fxml.Initializable {
+	
+	@FXML
+	TextField tfClientName;
+	
+	@FXML
+	TextField tfClientRfc;
+	
+	@FXML
+	TextField tfPaymentMethod;
+	
+	@FXML
+	TableView<?> tableViewInvoices;
+	
+	@FXML
+	Button btnGenerateInvoice;
 	
 	
 	
